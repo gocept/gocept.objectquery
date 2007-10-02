e5ec9d76b636e04213a4efdf84f6fa35422ffe9a
Sebastian Wehrmann <sebastian@wehrmann.de>
2007-10-02 07:18:20 +0000

    Some optimizations in parse function. Fixed bug in declaration (trailing / and | are now not alloed).     -> optimization: / and | are now recursive Added docstrings and descriptions. Added some more tests. Cleared sourcecode.

--- src/gocept/objectquery/pathexpressions.py
+++ src/gocept/objectquery/pathexpressions.py
@@ -35,11 +35,11 @@ class QueryProcessor(object):
         pass
 
 class QueryParser(object):
-
+    """ Parses a rpe query and returns a readable result. """
     def __init__(self):
         declaration = r'''
-        rpe             := ((bracket / normal), occurence?, (UNION /
-                           PATH_SEPARATOR)?)+
+        rpe             := expr, ((UNION / PATH_SEPARATOR), expr)?
+        expr            := ((bracket / normal), occurence?)+
         bracket         := open_bracket, rpe+, close_bracket
         normal          := PATH_SEPARATOR?, pathelem,
                            (PATH_SEPARATOR, pathelem)*
@@ -67,12 +67,44 @@ class QueryParser(object):
         self.parser = simpleparse.parser.Parser(declaration)
 
     def _modify_result(self, result, expression, output):
-        # stop condition
+        """ Modifies the SimpleParse result for better usability.
+
+            SimpleParse returns the same result format as the one from the
+            underlying mx.TextTools engine. This is for our approach not
+            usable, so we must convert it into a better one.
+
+            Imagine the following input query:
+
+                p.parse('foo/bar')
+
+            Here is what SimpleParse returns:
+
+                [('expr', 0, 7,
+                  [('normal', 0, 7,
+                    [('pathelem', 0, 3,
+                      [('ELEM', 0, 3, None)]),
+                     ('PATH_SEPARATOR', 3, 4, None),
+                     ('pathelem', 4, 7,
+                       [('ELEM', 4, 7, None)]
+                    )]
+                  )]
+                )]
+
+            And here, what we want and what _modify_result returns:
+
+                ['EEJOIN',
+                  ('ELEM', 'foo'),
+                  ('ELEM', 'bar')
+                ]
+        """
+
         if result is None:
             return None
         elif isinstance(result[0], basestring):
             if result[0] == "rpe":
                 return self._modify_result(result[3], expression, [])
+            elif result[0] == "expr":
+                return self._modify_result(result[3], expression, output)
             elif result[0] == "normal":
                 return self._modify_result(result[3], expression, output)
             elif result[0] == "bracket":
@@ -124,12 +156,11 @@ class QueryParser(object):
                 output = (self._modify_result(i, expression, output))
         return output
 
-
     def parse(self, expression):
         """ """
 
         if (expression is not None):
             succ, child, nextchar = self.parser.parse(expression, "rpe")
-            if (not succ or (nextchar != len(expression))):
-                raise SyntaxError("Wrong syntax in regular path expression")
+            if (not succ or nextchar != len(expression)):
+                raise SyntaxError("Wrong syntax in regular path expression.")
         return self._modify_result(child, expression, [])
