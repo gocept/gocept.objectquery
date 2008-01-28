41cb34b88d8a0f9a1fe78ac85ccda51154661dec
Sebastian Wehrmann <sebastian@wehrmann.de>
2008-01-28 10:54:53 +0000

    added support to query a list of objects

--- src/gocept/objectquery/nativequeries.py
+++ src/gocept/objectquery/nativequeries.py
@@ -3,4 +3,21 @@
 # $Id$
 
 class NativeQueries(object):
-    pass
+    """ Native Queries """
+
+    def __init__(self, database=None):
+        self.database = []
+        if database is not None:
+            self.database = database
+
+    def __call__(self, query):
+        self.response = []
+        for elem in self.database:
+            try:
+                if query(elem) == True:
+                    self.response.append(elem)
+            except (TypeError): pass
+        return self.response
+
+    def register_database(self, database):
+        self.database = database
