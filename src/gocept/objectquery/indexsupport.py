99c28c1a37f729d1d8b6048aa2b04178565352c5
Sebastian Wehrmann <sebastian@wehrmann.de>
2008-02-25 11:45:32 +0000

    - support for cycles and recursive adding/deleting of nodes in StructureIndex - refactoring

--- src/gocept/objectquery/indexsupport.py
+++ src/gocept/objectquery/indexsupport.py
@@ -69,8 +69,12 @@ class StructureIndex(OOIndex):
         super(self.__class__, self).__init__(dbroot)
         self.index['childs'] = OOBTree()    # Childindex, needed for deletion
 
-    def insert(self, key, parent):
+    def insert(self, key, parent=None):
         """ Insert key under parent. """
+        if key == 0:
+            raise ValueError("Key must be > 0.")
+        if parent is None:
+            parent = 0
         tail = (key, )
         new_path = []
         if self.index.has_key(parent):
@@ -86,7 +90,70 @@ class StructureIndex(OOIndex):
         self.index['childs'][parent].insert(key)
         if not self.index['childs'].has_key(key):
             self.index['childs'][key] = IndexItem()
+        for child in self.index['childs'][key]:
+            self._update_childs(child, new_path, [key])
         transaction.commit()
+
+    def delete(self, key, parent=None):
+        """ Delete the key from the index. """
+        if parent == 0:
+            parent = None
+        for elem in self.index[key][:]:
+            if parent is None or ( len(elem) > 1 and elem[-2] == parent ):
+                if len(elem) > 1 and key in self.index['childs'][elem[-2]]:
+                    self.index['childs'][elem[-2]].remove(key)
+                self.index[key].remove(elem)
+                self._purge(key, elem)
+        if len(self.index[key]) == 0:
+            del self.index[key]
+            del self.index['childs'][key]
+        transaction.commit()
+
+    def is_parent(self, key1, key2):
+        """ Check if key1 is a direct predecessor of key2. """
+        for elem1 in self.get(key1):
+            for elem2 in self.get(key2):
+                if len(elem1) + 1 == len(elem2) and\
+                    self._check_path(elem1, elem2):
+                    return True
+        return False
+
+    def is_child(self, key1, key2):
+        """ Check if key1 is a direct successor of key2. """
+        for elem1 in self.get(key1):
+            for elem2 in self.get(key2):
+                if len(elem1) == len(elem2) + 1 and\
+                    self._check_path(elem2, elem1):
+                    return True
+        return False
+
+    def is_predecessor(self, key1, key2):
+        """ Check if key1 is a predecessor of key2. """
+        for elem1 in self.get(key1):
+            for elem2 in self.get(key2):
+                if self._check_path(elem1, elem2):
+                    return True
+        return False
+
+    def is_successor(self, key1, key2):
+        """ Check if key1 is a successor of key2. """
+        for elem1 in self.get(key1):
+            for elem2 in self.get(key2):
+                if self._check_path(elem2, elem1):
+                    return True
+        return False
+
+    def _update_childs(self, key, path_dict, cycle_prevention):
+        """ Update childs of inserted nodes with the new path. """
+        tail = (key, )
+        new_path = []
+        for elem in path_dict:
+            new_path.append(elem + tail)
+        self.index[key].extend(new_path)
+        for child in self.index['childs'][key]:
+            if child not in cycle_prevention:
+                cycle_prevention.append(child)
+                self._update_childs(child, new_path, cycle_prevention)
 
     def _purge(self, key, tupel):
         """ Purge the child nodes of key with tupel. """
@@ -99,61 +166,17 @@ class StructureIndex(OOIndex):
                 if len(self.index[child]) == 0:
                     self.delete(child, key)
 
-    def delete(self, key, parent=None):
-        """ Delete the key from the index. """
-        if parent == 0:
-            parent = None
-        for elem in self.index[key][:]:
-            if parent is None or elem[-2] == parent:
-                if len(elem) > 1:
-                    self.index['childs'][elem[-2]].remove(key)
-                self.index[key].remove(elem)
-                self._purge(key, elem)
-        if len(self.index[key]) == 0:
-            del self.index[key]
-            del self.index['childs'][key]
-        transaction.commit()
-
     def get(self, key):
         """ Return the path for a given key. """
+        if key == 0:
+            raise ValueError("Key must be > 0.")
         return self.index[key]
 
-    def is_parent(self, key1, key2):
-        """ Check if key1 is a direct predecessor of key2. """
-        for elem1 in self.get(key1):
-            for elem2 in self.get(key2):
-                if len(elem1) + 1 == len(elem2):
-                    return True
-        return False
-
-    def is_child(self, key1, key2):
-        """ Check if key1 is a direct successor of key2. """
-        for elem1 in self.get(key1):
-            for elem2 in self.get(key2):
-                if len(elem1) == len(elem2) + 1:
-                    return True
-        return False
-
-    def is_predecessor(self, key1, key2):
-        """ Check is key1 is a predecessor of key2. """
-        for elem1 in self.get(key1):
-            for elem2 in self.get(key2):
-                size_of_elem1 = len(elem1)
-                if size_of_elem1 >= len(elem2):
-                    return False
-                for i in range(size_of_elem1):
-                    if elem1[i] != elem2[i]:
-                        return False
-        return True
-
-    def is_successor(self, key1, key2):
-        """ Check is key1 is a successor of key2. """
-        for elem1 in self.get(key1):
-            for elem2 in self.get(key2):
-                size_of_elem2 = len(elem2)
-                if len(elem1) <= size_of_elem2:
-                    return False
-                for i in range(size_of_elem2):
-                    if elem1[i] != elem2[i]:
-                        return False
+    def _check_path(self, path1, path2):
+        """ Check if path1 is reachable by path2. """
+        if len(path1) > len(path2):
+            return False
+        for i in range(len(path1)):
+            if path1[i] != path2[i]:
+                return False
         return True
