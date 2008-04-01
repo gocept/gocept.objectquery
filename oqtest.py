c7a245ee58038fb12f1441597902f0226e757e01
Sebastian Wehrmann <sebastian@wehrmann.de>
2008-04-01 15:00:27 +0000

    edit performance test script

--- oqtest.py
+++ oqtest.py
@@ -1,34 +1,55 @@
 import transaction, time
 from gocept.objectquery.testobjects import Dummy
 from ZODB import MappingStorage, DB
-from persistent import Persistent
 from gocept.objectquery.pathexpressions import RPEQueryParser
 from gocept.objectquery.collection import ObjectCollection
 from gocept.objectquery.processor import QueryProcessor
 
-def fill_db_width(n):
+def create_btree(n, c=1):
+    if n < 0:
+        raise ValueError("n should be >= 1")
+    if n == 0:
+        temp = Dummy(); temp.id = c
+        return temp
+    if n > 0:
+        left = create_btree(n-1, c)
+        right = create_btree(n-1, left.id+1)
+    temp = Dummy([left, right])
+    temp.id = right.id + 1
+    return temp
+
+heights = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+storage = MappingStorage.MappingStorage()
+db = DB(storage)
+conn = db.open()
+dbroot = conn.root()
+parser = RPEQueryParser()
+
+print " h    obj  |    bi     q1     q2     q3     q4     q5 "
+print "------------------------------------------------------"
+
+for n in heights:
     dbroot.clear()
-    objects = ObjectCollection(conn)
-    for i in xrange(n):
-        dbroot[i] = Dummy([Dummy([Dummy()]), Dummy()])
-        dbroot[i].id = i
+    oq = ObjectCollection(conn)
+    query = QueryProcessor(parser, oq)
+    objects = pow(2, n+1)-1
+
+    dbroot[0] = create_btree(n)
     transaction.commit()
-    t0 = time.clock()
-    for i in xrange(n):
-        objects.add(dbroot[i]._p_oid, dbroot._p_oid)
+    t0 = time.time()
+    oq.add(dbroot[0]._p_oid)
     transaction.commit()
-    t1 = time.clock()
-    print "Duration for inserting %i items: %is" %((n*4),(t1-t0))
-
-recsize = [250,1250,2500,12500,25000,50000,100000,150000,200000,250000]
-
-for n in recsize:
-    storage = MappingStorage.MappingStorage()
-    db = DB(storage)
-    conn = db.open()
-    dbroot = conn.root()
-    parser = RPEQueryParser()
-    objects = ObjectCollection(conn)
-    query = QueryProcessor(parser, objects)
-
-    fill_db_width(n)
+    t1 = time.time()
+    print "%2i %7i | %5.2fs" %(n, objects, (t1-t0)),
+    i = 1
+    queries = ['Dummy[@id="%i"]' %(objects/2), '/Dummy', '/Dummy/Dummy/Dummy',
+               'Dummy[@id="%i"]/_*/Dummy[@id="1"]' %objects,
+               'Dummy[@id<"1000"]']
+    for q in queries:
+        t0 = time.time()
+        r = query(q)
+        t1 = time.time()
+        print "%5.2fs" %(t1-t0),
+        i = i + 1
+    print ""
