ec3d43eee3ab42f36c0935c51d9b78e35e104066
Sebastian Wehrmann <sebastian@wehrmann.de>
2009-07-23 14:34:16 +0000

    Preparing release 0.1b

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ name = 'gocept.objectquery'
 
 setup(
     name=name,
-    version = '0.1a3dev',
+    version = '0.1b',
     url='https://intra.gocept.com/projects/projects/show/objectquery',
     license='ZPL 2.1',
     classifiers = [
