41b190e7acf015c8eaa1da31bf0ec5725f8c9933
Sebastian Wehrmann <sebastian@wehrmann.de>
2009-08-13 14:33:21 +0000

    Back to development: 0.1b2dev

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ name = 'gocept.objectquery'
 
 setup(
     name=name,
-    version = '0.1b1',
+    version = '0.1b2dev',
     url='https://intra.gocept.com/projects/projects/show/objectquery',
     license='ZPL 2.1',
     classifiers = [
